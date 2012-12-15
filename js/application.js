d94a6d28efa598aa12b3a3ab4cd3bd0ed4707797
Selva <k.n.selvakumar@gmail.com>
2012-12-15 15:31:13 +0530

    SriV/Selva Add user and tags, Find users startingWith

---
+++ js/application.js
@@ -0,0 +1,35 @@
+var connectToDB = function(callback){
+	rethinkdb.connect({ host: '10.16.4.39', port: 8080 }, function(conn){
+		console.log("Connected to DB");
+		conn.use('passionfruit');
+		var table = rethinkdb.table('passion')
+		callback(table, rethinkdb, conn);
+		conn.close();
+	}, 
+	function(){
+		alert("Not able to connect to db");
+	});
+}
+
+var namesStartingWith = function(startingWith, callback) {
+	connectToDB(function(table, r) {
+		table
+			.filter(r.js("this.user.indexOf('" +  startingWith + "') == 0"))
+			.map(function(passion){return passion('user')})
+			.run().collect(callback);
+	});
+}
+
+$(document).ready(function(){
+	$("#add-tags-form").submit(function(e){
+		e.preventDefault();
+		var formValues = $(e.target).serializeObject();
+		var user = formValues["name"];
+		var tags = formValues["tags"];
+		connectToDB(function(table){
+			table.insert({ user: user, tags: tags.split(",") }).run();
+		});
+	});
+
+
+});
